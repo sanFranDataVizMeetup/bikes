5d13626c1798a24822c97d8eff6dc8664df148bc
Erispoe <favrebulle@gmail.com>
2014-10-26 17:03:09 -0700

    Get weather data
    Only print it for the moment

---
+++ data/addWeather.py
@@ -0,0 +1,26 @@
+# A python function adding the historical weather conditions to the bike accident JSON file
+
+def addWeather(data):
+	import datetime
+	import json
+	import urllib2
+	cityName = "San Francisco, USA"
+
+	print len(data)
+
+	for accident in data:
+		# Format and decompose the date in year, month and day
+		date = accident['date']
+		date = date[-8:]
+		y = date[0:4]
+		m = date[4:6]
+		d = date[6:8]
+
+		apiKey = str(json.loads(open('apikey.secret.config').read())['apikey'])
+
+		url = "http://api.wunderground.com/api/" + apiKey + "/history_" + y + m + d + "/q/CA/San_Francisco.json"
+
+		res = json.loads(urllib2.urlopen(url).read())
+
+		from pprint import pprint
+		pprint(res)
