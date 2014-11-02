2f204438e34604e078451711c3dfe90cbc41cdf0
Becka <rebecca.robbins.et@gmail.com>
2014-11-02 09:19:20 -0800

    fixed small error so it correctly iterates

--- data/addDay.py
+++ data/addDay.py
@@ -1,11 +1,25 @@
+
 # A python function adding the day, and type of day to the bike accident JSON file
+# open file
+import json
+from pprint import pprint
+open('new_crash_data.json', 'w')
+with open('sanfrancisco_crashes_cp.json', 'rw') as json_data:
+	json_data = json.load(json_data)
+	accidents = json_data['accidents']
+	#pprint(data)
+
+def convertDate(data):
+	for accident in data:
+		date = accdent['date']
+
+
 
 def addDay(data):
 	import datetime
 	newdata = []
 
 	for accident in data:
-		
 		# Format and decompose the date in year, month and day
 		date = accident['date']
 		date = date[-8:]
@@ -18,6 +32,8 @@ def addDay(data):
 
 		# Create new fields in the accident object
 		accident[u'date'] = date
+		date = date[2:]
+		date = int(date)
 		accident[u'weekday'] = dt.weekday()
 		accident[u'weekdayname'] = weekdayname(accident[u'weekday'])
 
@@ -31,9 +47,10 @@ def addDay(data):
 			accident[u'weekdaytypename'] = u'Weekend day'
 
 		newdata.append(accident)
-
 	return newdata
 
 def weekdayname(i):
     days = [u'Monday', u'Tuesday', u'Wednesday', u'Thursday', u'Friday', u'Saturday', u'Sunday']
     return days[i]
+
+print addDay(accidents)
